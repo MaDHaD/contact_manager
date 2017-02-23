578278b42c177cd488b52345e7e27b7938aaf83c
TOR <geridrwatr@gmail.com>
2017-02-23 13:14:03 +0200

    de

--- view/delete.php
+++ view/delete.php
@@ -5,24 +5,6 @@ require_once('../app/delete_app.php');
 
 ?>
 
-
-<html>
-<head>
-
-</head>
-<body>
-<form action="delete.php?id=<?php echo $_GET['id']?>" method="post">
-    <div align="center">Are you sure<br/>
-        <input type="submit" name="ok" value="YES">
-        <a href="../index.php">NO</a>
-    </div>
-
-</form>
-
-
-
-
-
 <html>
 <head>
 
